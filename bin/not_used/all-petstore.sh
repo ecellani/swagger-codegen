3412734cd18676389f54352e7c0e939edc90dfd5
Erick Cellani <erick.cellani@walmart.com>
2015-03-13 11:34:52 -0300

    SEM-918: Jarvis

---
+++ bin/not_used/all-petstore.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+cd $APP_DIR
+./bin/android-java-petstore.sh
+./bin/dynamic-html.sh
+./bin/html.sh
+./bin/jaxrs-petstore-server.sh
+./bin/java-petstore-filemap.sh
+./bin/java-petstore.sh
+./bin/php-petstore.sh
+./bin/python-petstore.sh
+./bin/objc-petstore.sh
+./bin/scala-petstore.sh
+./bin/tizen-petstore.sh
